503070c5d06b9a13fef1689aadac1cd80cac9f71
Hwan_laptop <tjsgkrtks012@naver.com>
2020-10-11 17:47:37 +0900

    heckto

---
+++ 4th-week/Test/src/Test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Test {
+
+    public static void main(String args[])
+    {
+        Queue<String> pq = new PriorityQueue<>();
+
+        pq.add("Geeks");
+        pq.add("For");
+        pq.add("Geeks");
+
+        System.out.println("Initial Queue " + pq);
+
+        pq.remove("Geeks");
+
+        System.out.println("After Remove " + pq);
+
+        System.out.println("Poll Method " + pq.poll());
+
+        System.out.println("Final Queue " + pq);
+    }
+} 
